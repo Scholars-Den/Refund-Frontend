359064b7fdf3b60b3889f864c4b1b38bc88a5a03
Scholars Digital <scholarsdigital@Mac.mshome.net>
2025-07-15 13:43:44 +0530

    add cookie

---
+++ utils/cookieUtils.js
@@ -0,0 +1,9 @@
+// utils/cookieUtils.js
+export const getCookie = (name) => {
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split("=");
+    if (key === name) return decodeURIComponent(value);
+  }
+  return null;
+};
